77066d01fdb363f57a2ffa257b30edb318df27e6
EvanBagis <evanbagis@gmail.com>
2024-01-23 17:21:04 +0200

    changes in advanced_forecasting.py

--- advanced_forecasting.py
+++ advanced_forecasting.py
@@ -31,7 +31,137 @@ from moviepy.editor import VideoClip
 from moviepy.video.io.bindings import mplfig_to_npimage
 from tqdm import tqdm
 
+warnings.simplefilter(action='ignore', category=ConvergenceWarning)
+warnings.filterwarnings("ignore")
+
 plt.rcParams.update({'font.size': 22})
+
+# Functions
+
+def display_metrics(true, pred, returns=False):
+    """Displays or returns the regression metrics.
+    ##ARGS:
+    
+        true:<<list or np.array or pd.series>> the measurements
+        
+        pred:<<list or np.array or pd.series>> the predictions
+        
+    ##RETURNS
+        
+        returns: <<boolean>> specifies if the metrics are to be returned or displayed
+    """
+    def index_agreement(s, o):
+        """
+        This function calculates the index of agreement between two arrays.
+        It is a measure of the accuracy of a model prediction compared to observed data.
+        The function expects two arrays of equal length.
+        The function returns a float representing the index of agreement.
+        """
+        ia = 1 - (np.sum((o - s) ** 2)) / (
+            np.sum((np.abs(s - np.mean(o)) + np.abs(o - np.mean(o))) ** 2))
+        return ia
+    
+    rmse = np.sqrt(mean_squared_error(true, pred))
+    mae = mean_absolute_error(true, pred)
+    r = pearsonr(true, pred)[0]
+    rs = spearmanr(true, pred)[0]
+    r2 = r2_score(true, pred)
+    MBE = np.mean(true-pred)
+    ia = index_agreement(pred, true)
+
+    if returns:
+        return rmse, mae, r2, r, rs, MBE, ia
+    else:
+        print(f"RMSE = {rmse:.2f}, MAE = {mae:.2f}, r2 = {r2:.2f}, Pearson = {r:.2f}, Spearman = {rs:.2f}, MBE = {MBE:.2f}, IA = {ia:.2f}")
+        return 0
+
+# Transform wind to degrees
+def wind_dir(di):
+    if di == 'N':
+        di = 0
+    elif di == 'NNE':
+        di = 22.5
+    elif di == 'NE':
+        di = 45
+    elif di == 'ENE':
+        di = 67.5
+    elif di == 'E':
+        di = 90
+    elif di == 'ESE':
+        di = 112.5
+    elif di == 'SE':
+        di = 135
+    elif di == 'SSE':
+        di = 157.5
+    elif di == 'S':
+        di = 180
+    elif di == 'SSW':
+        di = 202.5
+    elif di == 'SW':
+        di = 225
+    elif di == 'WSW':
+        di = 247.5
+    elif di == 'W':
+        di = 270
+    elif di == 'WNW':
+        di = 292.5
+    elif di == 'NW':
+        di = 315
+    elif di == 'NNW':
+        di = 337.5
+    return di
+
+# Transform degrees to sinusoidal feature
+def transform(angle):
+    return np.sin(angle * np.pi / 180)
+
+# Create more features
+def feature_engineering(data, lags, window_sizes):
+    """
+    Perform feature engineering on a Pandas DataFrame by calculating rolling statistics
+    and creating lag features.
+
+    ## ARGS:
+    - data (pd.DataFrame): Input DataFrame containing the original data.
+    - lags (int): Number of lag features to create for each column in the DataFrame.
+    - window_sizes (list of int): List of window sizes for calculating rolling statistics.
+
+    ## RETURNS:
+    - If returns is True, returns a new DataFrame with added features.
+    - If returns is False, modifies the input DataFrame in place.
+
+    Example:
+    >>> new_data = feature_engineering(df, 12, [24, 48])
+
+    Notes:
+    - This function creates various rolling statistics (mean, std, min, max, median,
+      min-max difference) for each column in the input DataFrame based on the specified
+      window sizes.
+    - It also generates lag features for each column up to the specified number of lags.
+    - The resulting DataFrame may contain NaN values for rows where features could not
+      be calculated due to insufficient historical data or lag values.
+    - If returns is set to True, the function returns a new DataFrame with added features.
+      If returns is set to False, the function modifies the input DataFrame in place.
+    """
+
+    # create denoised features with rolling statistical moments
+    data2 = data.copy()
+    for col in data.columns:
+        for window in window_sizes:
+            data2["rolling_mean_" + col + '-' + str(window)] = data[col].rolling(window=window).mean()
+            data2["rolling_std_" + col + '-' + str(window)] = data[col].rolling(window=window).std()
+            data2["rolling_min_" + col + '-' + str(window)] = data[col].rolling(window=window).min()
+            data2["rolling_max_" + col + '-' + str(window)] = data[col].rolling(window=window).max()
+            data2["rolling_median_" + col + '-' + str(window)] = data[col].rolling(window=window).median()
+            data2["rolling_min_max_diff_" + col + '-' + str(window)] = data2["rolling_max_" + col + '-' + str(window)] - \
+                                                                       data2["rolling_min_" + col + '-' + str(window)]
+    # create lag features
+    cols = data.columns.to_list()
+
+    for col in cols:
+        for i in range(1, lags):
+            data2[f'{col}_lag_{i}'] = data[col].shift(i)
+    return data2.dropna()
 
 def make_forecast_video(n_station, y_test, y_pred):
     '''
@@ -69,15 +199,7 @@ def make_forecast_video(n_station, y_test, y_pred):
     animation.write_gif(results_path + f"{target}/station={n_station}.gif", fps=1)
     #animation.ipython_display(fps=30, loop=False, autoplay=True, maxduration=12000)
 
-warnings.simplefilter(action='ignore', category=ConvergenceWarning)
 
-def display_metrics(true, pred, returns=False):
-
-    if returns:
-        return f"RMSE = {np.sqrt(mean_squared_error(true, pred)):.2f}, MAE = {mean_absolute_error(true, pred):.2f}, r2 = {r2_score(true, pred):.2f}, R = {pearsonr(true, pred)[0]:.2f}"
-    else:
-        print(f"RMSE = {np.sqrt(mean_squared_error(true, pred)):.2f}, MAE = {mean_absolute_error(true, pred):.2f}, r2 = {r2_score(true, pred):.2f}, R = {pearsonr(true, pred)[0]:.2f}")
-        return 0
     
 def denoise(series, sm):
     if sm == "conv":
@@ -132,9 +254,24 @@ def create_x_y_raw(path, enf_target, n_stations, future, num_feats, sm):
     x = np.stack(x); y = np.stack(y); raw_y = np.stack(raw_y)
     return x, y, raw_y
 
-results_path = f"/var/snap/docker/common/var-lib-docker/volumes/fmi-vol/_data/forecast_results/"
-path = "/var/snap/docker/common/var-lib-docker/volumes/fmi-vol/_data/FMI_ref/" # docker
-#path = '/media/evan/5a4571f6-ff19-4d1f-a2f9-f74087bdf128/FMI/FMI_ref/' # local
+path = '/home/evan/venv/Beijing_Air_Quality_Forecasting/raw_data/'
+files = sorted(os.listdir(path))
+
+# load and concatenate all the files into a single dataframe
+dfs = pd.concat([pd.read_csv(path + file, index_col=0) for file in files])
+
+# reconstruct the datetime index
+dfs.index = pd.to_datetime(dfs[['year', 'month', 'day', 'hour']])
+dfs = dfs.drop(['year', 'month', 'day', 'hour'], axis=1)
+
+# transform wind directions into floats and transform the column
+dfs['wd'] = dfs['wd'].apply(wind_dir).apply(transform)
+
+print(dfs)
+
+
+
+'''
 targets = ["coarsePM", "PM25", "PM10", "O3", "NO2"]
 num_stations = [11, 11, 12, 4, 11]
 future = 12; num_feats = 12
@@ -196,5 +333,5 @@ for target, n_stations in zip(targets, num_stations):
     #    pd.DataFrame(np.hstack([p2, t2])).plot()
     #    plt.show()
     
-    
+    '''
 
